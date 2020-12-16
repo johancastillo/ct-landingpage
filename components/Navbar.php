3a7068b8b184ec16b3b41df2405e32a7bf632945
Johan Castillo <jcjohan2707@gmail.com>
2020-12-16 12:24:30 -0400

    Sections

--- components/Navbar.php
+++ components/Navbar.php
@@ -1,14 +1,14 @@
 <header>
-    <nav>
+    <nav class="navbar">
         <div>
             <img src="./../assets/img/logo.svg" alt="">
         </div>
 
         <ul>
-            <li><a href="#section1">Enlace 1</a></li>
-            <li><a href="#section2">Enlace 2</a></li>
-            <li><a href="#section3">Enlace 3</a></li>
-            <li><a href="#section4">Enlace 4</a></li>
+            <li><a href="#section1">Section 1</a></li>
+            <li><a href="#section2">Section 2</a></li>
+            <li><a href="#section3">Section 3</a></li>
+            <li><a href="#section4">Section 4</a></li>
         </ul>
     </nav>
 </header>
--- index.php
+++ index.php
@@ -7,14 +7,29 @@
     <link rel="shortcut icon" href="./assets/img/icon.svg" type="image/x-icon">
     <title>Tarjeta Efectivo</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/main.css">
 </head>
 <body>
+    <!-- Navbar -->
     <?php include "./components/Navbar.php" ?>
 
-    <div class="box" id="section1"></div>
-    <div class="box" id="section2"></div>
-    <div class="box" id="section3"></div>
+    <!-- Sections -->
+    <div class="box" id="section1">
+        <h1 class="text-uppercase">Section 1</h1>
+    </div>
+
+    <div class="box" id="section2">
+        <h1 class="text-uppercase">Section 2</h1>
+    </div>
+
+    <div class="box" id="section3">
+        <h1 class="text-uppercase">Section 3</h1>
+    </div>
+
+    <div class="box" id="section4">
+        <h1 class="text-uppercase">Section 4</h1>
+    </div>
     
 </body>
 </html>
