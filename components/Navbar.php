6b1981cbb64bb1599675db444d5dbeebeb295cb8
Johan Castillo <jcjohan2707@gmail.com>
2020-12-16 11:55:11 -0400

    Structured Basic of the project

---
+++ components/Navbar.php
@@ -0,0 +1,14 @@
+<header>
+    <nav>
+        <div>
+            <img src="./../assets/img/logo.svg" alt="">
+        </div>
+
+        <ul>
+            <li><a href="#section1">Enlace 1</a></li>
+            <li><a href="#section2">Enlace 2</a></li>
+            <li><a href="#section3">Enlace 3</a></li>
+            <li><a href="#section4">Enlace 4</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es-ve">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="./assets/img/icon.svg" type="image/x-icon">
+    <title>Tarjeta Efectivo</title>
+
+    <link rel="stylesheet" href="./assets/css/main.css">
+</head>
+<body>
+    <?php include "./components/Navbar.php" ?>
+
+    <div class="box" id="section1"></div>
+    <div class="box" id="section2"></div>
+    <div class="box" id="section3"></div>
+    
+</body>
+</html>
