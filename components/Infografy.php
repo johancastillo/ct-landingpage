673a2a7647d8f54f994e5b304bfe440e71bc7ddd
Johan Castillo <jcjohan2707@gmail.com>
2020-12-24 13:56:53 -0400

    Add Design

--- components/Infografy.php
+++ components/Infografy.php
@@ -1,1 +1,2 @@
-<img src="./assets/img/banner.webp" alt="" class="mt-4">
+<img src="./assets/img/banner-desktop.webp" alt="" class="mt-4 d-none d-sm-block">
+<img src="./assets/img/banner-mobile.webp" alt="" class="mt-4 d-block d-sm-none">
--- components/Navbar.php
+++ components/Navbar.php
@@ -30,15 +30,11 @@
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link scroll" href="#screenshots">Screenshots</a>
-                        </li>
-
-                        <li class="nav-item">
                             <a class="nav-link scroll" href="#questions">Preguntas</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link scroll" href="#contact">Contact</a>
+                            <a class="nav-link scroll" href="">Testimonios</a>
                         </li>
                     </ul>
                 </nav>
