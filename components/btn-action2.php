3fabfbacc21e75e7db04525a2930839fffeb3135
Johan Castillo <jcjohan2707@gmail.com>
2020-12-28 21:07:23 -0400

    Buttons responsive

--- components/btn-action2.php
+++ components/btn-action2.php
@@ -1,11 +1,11 @@
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row buttons">
 
         <button class="btn bg-tertiary" onclick="redirect()">
             Compra para ti
         </button>
 
-        <button class="btn bg-primary ml-4 efectivo" onclick="redirect()">
+        <button class="btn bg-primary efectivo" onclick="redirect()">
             <span class="icon-gift"></span> ¡Regala!
         </button>
 
